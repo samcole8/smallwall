8a800adbc6ee801468029d545c2ea135a8081844
Sam Cole <sam@samcole.me>
2024-01-30 23:56:49 +0000

    Update smallwall.py to reflect default.toml changes

--- smallwall/smallwall.py
+++ smallwall/smallwall.py
@@ -9,15 +9,18 @@ SMALLWALL = (
     "smallwall\n"
     )
 
+# Relative path to configuration file
 CONFIG_RPATH = "smallwall.toml"
+
+
 MOUNT_RPATH = "/mnt/smallwall"
 MOUNT_CONFIG_PATH = "/etc/nftables.conf"
 
 def gen(config):
 
-    qz = config["firewall"]["qz"]["if"]
-    lan = config["firewall"]["lan"]["if"]
-    netid_cidr = f'{config["firewall"]["lan"]["id"]}/{config["firewall"]["lan"]["cidr"]}'
+    qz = config["firewall"]["qz"]["interface"]
+    lan = config["firewall"]["lan"]["interface"]
+    netid_cidr = f'{config["firewall"]["lan"]["network_id"]}{config["firewall"]["lan"]["cidr"]}'
     gateway = config["firewall"]["lan"]["gateway"]
 
     return (
@@ -43,17 +46,17 @@ def gen(config):
         "}\n"
     )
 
-def deploy(chroot_path, chroot_config):
+def deploy(config):
     # Generate nftables config file
-    nftables_to_write = gen(chroot_config)
+    nftables_to_write = gen(config)
     # Overwrite nftables config file on mount
     try:
-        if f"{chroot_path}{MOUNT_CONFIG_PATH}" != "/etc/nftables":
-            with open(f'{chroot_path}{MOUNT_CONFIG_PATH}', "w") as nftables_config:
+        if f'{config["filesystem"]["mountpoint"]}{config["filesystem"]["nftables"]}' != "/etc/nftables":
+            with open(f'{config["filesystem"]["mountpoint"]}{config["filesystem"]["nftables"]}', "w") as nftables_config:
                 nftables_config.write(nftables_to_write)
-            helpers.log(f"INFO: Successfully written nftables configuration to {chroot_path}{MOUNT_CONFIG_PATH}.")
+            helpers.log(f'INFO: Successfully written nftables configuration to {config["filesystem"]["mountpoint"]}{config["filesystem"]["nftables"]}.')
     except FileNotFoundError:
-        helpers.log(f"FATAL: Cannot find {chroot_path}{MOUNT_CONFIG_PATH}.")
+        helpers.log(f'FATAL: Cannot find {config["filesystem"]["mountpoint"]}{config["filesystem"]["nftables"]}.')
 
 def smallwall(device):
     print(SMALLWALL)
@@ -62,11 +65,11 @@ def smallwall(device):
     # Load config
     config = helpers.load_toml(CONFIG_RPATH, auto_create=True, skeleton="default.toml")
     # Mount disk
-    helpers.mount("m", device, MOUNT_RPATH)
-    # Modify iptables    
-    deploy(MOUNT_RPATH, config)
+    helpers.mount("m", device, config["filesystem"]["mountpoint"])
+    # Modify iptables
+    deploy(config)
     # Unmount disk
-    helpers.mount("u", device, MOUNT_RPATH)
+    helpers.mount("u", device, config["filesystem"]["mountpoint"])
     # Cleanup - remove mount dir
 
 if __name__ == "__main__":
